32c21e046d536a9a09f1e8c5f2776edc4fccbd82
Grant Raisig <gfraisig@outlook.com>
2019-04-14 11:57:20 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+def turn_count(board)
+
+  count = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      count += 1
+    end
+  end
+  
+  return count
+end
+
+def current_player(board)
+  
+  num_turns = turn_count(board)
+  
+  if num_turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+  
+end
